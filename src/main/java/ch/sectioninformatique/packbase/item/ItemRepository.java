85a2726ed32827007185bb24d6affa641e577dfe
Didier Viret <didier.viret@orif.ch>
2024-11-06 15:44:34 +0100

    Add all Item layers

--- src/main/java/ch/sectioninformatique/packbase/item/ItemRepository.java
+++ src/main/java/ch/sectioninformatique/packbase/item/ItemRepository.java
@@ -1,5 +1,12 @@
 package ch.sectioninformatique.packbase.item;
 
-public class ItemRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/* @Repository annotation indicates that the class is a Bean communicating with a data source
+ * CrudRepository is a Spring provided interface.
+ */
+@Repository
+public interface ItemRepository extends CrudRepository<Item, Long> {
 
 }
